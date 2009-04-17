ae1d4a12c14f4a32cafd3e0bb4b363b4e0308380
john <git@johnmuhl.com>
2009-04-17 17:09:42 -0600

    version 2.1

--- ray_extension.rb
+++ ray_extension.rb
@@ -2,20 +2,20 @@
 # require_dependency 'application'
 
 class RayExtension < Radiant::Extension
-  version "2.0"
+  version "2.1"
   description "Friendly extension management"
   url "http://wiki.github.com/johnmuhl/radiant-ray-extension"
-  
+
   # define_routes do |map|
   #   map.connect 'admin/ray/:action', :controller => 'admin/ray'
   # end
-  
+
   def activate
     # admin.tabs.add "Ray", "/admin/ray", :after => "Layouts", :visibility => [:admin]
   end
-  
+
   def deactivate
     # admin.tabs.remove "Ray"
   end
-  
+
 end
